053ff5e479fa79eac6dbfb259d08a873bb889bd0
Subrata Guchhait <sguchhaitdev@gmail.com>
2024-06-17 06:17:25 +0530

    Implemet route and auth

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,13 @@
+// import React from "react";
+
+// const SignUp = () => {
+//   return <div>Sign-Up</div>;
+// };
+
+// export default SignUp;
+
+import { SignUp } from "@clerk/nextjs";
+
+export default function Page() {
+  return <SignUp />;
+}
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">We are Comming</h1>
-      <h2 className="h2-bold">We are Comming</h2>
-      <h3 className="h3-bold">We are Comming</h3>
-    </div>
-  );
-}
