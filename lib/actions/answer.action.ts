e15ff2050476910e05df54742cf955ecafc6331e
Subrata Guchhait <sguchhaitdev@gmail.com>
2024-07-17 10:47:52 +0530

    Implement Reputation && Badge System

--- lib/actions/answer.action.ts
+++ lib/actions/answer.action.ts
@@ -6,6 +6,7 @@ import { AnswerVoteParams, CreateAnswerParams, DeleteAnswerParams, GetAnswersPar
 import Question from "@/database/question.model";
 import { revalidatePath } from "next/cache";
 import Interaction from "@/database/interaction.model";
+import User from "@/database/user.model";
 
 
 export async function createAnswer(params: CreateAnswerParams){
@@ -17,9 +18,19 @@ export async function createAnswer(params: CreateAnswerParams){
         const newAnswer = await Answer.create({content,author,question})
 
         // Add the answer to the question's answer array
-        await Question.findByIdAndUpdate(question, {
+       const questionObject = await Question.findByIdAndUpdate(question, {
             $push: {answers: newAnswer._id}
         })
+
+        await Interaction.create({
+          user: author,
+          action: "answer",
+          question,
+          answer: newAnswer._id,
+          tags: questionObject.tags
+        })
+
+        await User.findByIdAndUpdate(author, {$inc: {reputation: 10}})
 
         revalidatePath(path)
     } catch (error) {
@@ -99,6 +110,9 @@ export async function upvoteAnswer(params: AnswerVoteParams) {
       }
   
       // Increment author's reputation
+
+      await User.findByIdAndUpdate(userId, {$inc: {reputation: hasupVoted ? -2: 2}})
+      await User.findByIdAndUpdate(answer.author, {$inc: {reputation: hasupVoted ? -10: 10}})
   
       revalidatePath(path);
     } catch (error) {
@@ -134,6 +148,9 @@ export async function upvoteAnswer(params: AnswerVoteParams) {
       }
   
       // Increment author's reputation
+      await User.findByIdAndUpdate(userId, {$inc: {reputation: hasdownVoted ? -2: 2}})
+      await User.findByIdAndUpdate(answer.author, {$inc: {reputation: hasdownVoted ? -10: 10}})
+  
   
       revalidatePath(path);
     } catch (error) {
